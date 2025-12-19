4b941b598120b23168932a602495a5b01438a52e
valecristiano <valeriamdcristiano@gmail.com>
2025-12-19 16:54:29 +0300

    initial commit

---
+++ Dadi/README.md
@@ -0,0 +1,12 @@
+# Gioco dei dadi
+
+- Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+- Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+## Consigli del giorno:
+
+- scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+- javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+- si ma noi cosa vogliamo fare?
+- torniamo a scrivere in italiano
+- proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
---
+++ Mail/js/scriptmail.js
@@ -0,0 +1,26 @@
+// Crea una lista di email di invitati ad una festa.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+// Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’.
+
+// creare una lista di mail con un array
+
+const invitedList = [
+  "mario.rossi@example.com",
+  "luca.bianchi@testmail.com",
+  "anna.verdi@fakemail.org",
+  "giulia.neri@sample.net",
+  "paolo.conti@demoemail.com",
+  "sara.ferrari@mockmail.io",
+  "andrea.russo@tempmail.net",
+  "chiara.esposito@placeholder.org",
+  "francesco.gallo@dummyemail.com",
+  "valentina.mancini@testbox.io",
+];
+console.log(invitedList);
+
+//creare un prompt per cheidere email all'utente
+let useremail = prompt("Scrivi qui la tua email");
+//creare un for che controlli se l'email dell'utente è presente o meno nella lista
+// creare un otput che stampa (o alert) un messaggio positivo o negativo in base all'esito del controllo
